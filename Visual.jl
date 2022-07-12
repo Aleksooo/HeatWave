0a7d1a24a03f1107a4223521236e56c8a16faf54
Alexey <alexey@MacBook-Pro.local>
2022-07-12 16:31:23 +0300

    Добавлены новые функции визуализации полученного решения

--- Visual.jl
+++ Visual.jl
@@ -1,16 +1,33 @@
-function DrawSol(Deq::DEquation; t::Int)
+function DrawPlot(Deq::DEquation; t::Int)
     if t > Deq.T+1
         error("Time out of range!")
     end
     x = range(0, Deq.N*Deq.dx, Deq.N+1)
+
     plot(x, Deq.SolMat[t, :])
 end;
 
-function DrawAnim(Deq::DEquation; FPS::Int)
+function DrawPlotAnim(Deq::DEquation; FPS::Int=10, step::Int=1)
     x = range(0, Deq.N*Deq.dx, Deq.N+1)
-    anim = @animate for t = 1:100:Deq.T+1 
-        plot(x, Deq.SolMat[t, :], title=string(t*Deq.dt))
+    anim = @animate for t = 1:step:Deq.T+1 
+        plot(x, Deq.SolMat[t, :], title=string(round(t*Deq.dt; digits=3)), legend=false)
     end
 
-    gif(anim, "TempWave.gif", fps=FPS)
-end
+    gif(anim, "TempWavePlot.gif", fps=FPS)
+end;
+
+function DrawHeatmap(Deq::DEquation; t::Int)
+    data = Deq.SolMat[t, :]
+
+    heatmap([data;; data]')
+end;
+
+function DrawHeatmapAnim(Deq::DEquation; FPS::Int=10, step::Int=1)
+    x = range(0, Deq.N*Deq.dx, Deq.N+1)
+    anim = @animate for t = 1:step:Deq.T+1 
+        data = Deq.SolMat[t, :]
+        heatmap([data;; data]', title=string(round(t*Deq.dt; digits=3)))
+    end
+
+    gif(anim, "TempWaveMap.gif", fps=FPS)
+end;
